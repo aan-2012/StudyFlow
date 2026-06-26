8b7d7c2470e19022eb526f21f19a9372de343f40
Ansh Nadger <anshnadger@gmail.com>
2026-06-26 13:06:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "tasks.db")
+DB_PATH = "/tmp/tasks.db"
 
 def init_db():
     conn = sqlite3.connect(DB_PATH)
